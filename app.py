4eb38c8be954ae258c281c6ce5495879f77a213d
David Waweru <wawerudave02.com>
2026-05-04 19:16:19 +0300

    Initial commit

--- app.py
+++ app.py
@@ -232,5 +232,6 @@ def index():
 # -------------------------------
 # RUN APP
 # -------------------------------
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5012)))
